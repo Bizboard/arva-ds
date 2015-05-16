631c81585b1113939a9c9002a20212ea07405bbc
Hans van den Akker <hans@bizboard.nl>
2015-05-16 10:46:48 +0200

    becoming jspm 0.15 compliant.

--- config.js
+++ config.js
@@ -10,13 +10,19 @@ System.config({
 System.config({
   "map": {
     "arva-context": "github:Bizboard/arva-context@master",
-    "di.js": "github:angular/di.js@master",
+    "di.js": "github:Bizboard/arva-ds@master",
     "firebase": "github:firebase/firebase-bower@2.2.4",
-    "lodash": "npm:lodash@3.7.0",
+    "lodash": "npm:lodash@3.8.0",
+    "github:Bizboard/arva-ds@master": {
+      "arva-context": "github:Bizboard/arva-context@master",
+      "di.js": "github:angular/di.js@master",
+      "firebase": "github:firebase/firebase-bower@2.2.4",
+      "lodash": "npm:lodash@3.8.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:lodash@3.7.0": {
+    "npm:lodash@3.8.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
