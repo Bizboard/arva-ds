5afd7616da1a8d0951ca5af9244c9c65a22f27cf
Hans van den Akker <hans@bizboard.nl>
2015-05-18 16:10:10 +0200

    finding my way in Tom's World.

--- src/core/Model.js
+++ src/core/Model.js
@@ -57,7 +57,7 @@ export default class Model extends PrioritisedObject {
             if (options.dataSource) { dataSource = options.dataSource; }
             else if (options.path) { dataSource = dataSource.child(options.path); }
             else dataSource = dataSource.child(pathRoot).child(id);
-            if (data) dataSource.set(data);
+            //if (data) dataSource.set(data);
         } else {
             /* No id is present, check if we have a dataSnapshot we can extract it from.
              * If we can't, generate a random one by pushing a new entry to the dataSource. */
