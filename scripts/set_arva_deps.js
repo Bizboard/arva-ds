ebf8b259cff1a3308965f0645f54dbd741682079
Tom Clement <mr@tomclement.nl>
2015-10-15 10:23:13 +0200

    Added node script to change the version of Arva dependencies

---
+++ scripts/set_arva_deps.js
@@ -0,0 +1,32 @@
+/**
+ * Created by tom on 15/10/15.
+ */
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+var arvaDependencies = [
+    'SPSoapAdapter',
+    'arva-utils'
+];
+
+
+var version = process.argv[3];
+if(!version) {
+    console.log('No version to set arva dependencies to was given. \r\n' +
+        'Example usage: `node scripts/set_arva_deps.js -- master` to set arva dependencies to @master');
+    process.exit(1);
+}
+
+(function installDependency(){
+    var dependency = arvaDependencies.shift();
+    if(!dependency) { return; }
+
+    exec(`jspm install ${dependency}=github:bizboard/${dependency}@${version}`, function(error, stdout, stderr) {
+        sys.puts(stdout);
+        if(error){
+            console.log('Error whilst installing dependency:', error);
+        }
+        installDependency();
+    });
+})();
