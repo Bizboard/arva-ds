08f928786a69133e712b4800d79ca60abfaaac59
Hans van den Akker <hans@bizboard.nl>
2016-03-04 14:35:47 +0100

    arrows don't work with map.

--- src/datasources/SharePoint/DataModelGenerator.js
+++ src/datasources/SharePoint/DataModelGenerator.js
@@ -95,7 +95,7 @@ export class DataModelGenerator {
 
             // update
             let viewId = this._ResolveViewID(viewResult.response);
-            let fieldNames = fields.map((field)=>  field.name);
+            let fieldNames = fields.map(function (field) { return field.name; });
             let updateRequest = this._getUpdateViewRequest(listName, viewId, fieldNames);
             let updateResult = await PostRequest(updateRequest);
 
